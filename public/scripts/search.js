d21e5c2e17a5ba6b6cf8b04e77259a461864f12e
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-10-21 14:15:25 -0700

    Add card styling and heart icon to search results

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,24 +1,26 @@
+console.log('search js connected');
 
 // user submits a search query
 const onSuccess = (res) => {
   const $searchResults = $('#searchResults');
   $searchResults.empty();
   res.results.forEach((result) => {
-  const temp = `
-    <div class="container">
-      <div class="card">
-        <div class="search-result card-body">
-          <div>
-          <img class="result-img" src="${result.artworkUrl600}" />
-          <div class="podcast-name">${result.collectionName}</div>
+  const temp = `<div class="col-sm-6">
+    <div class="card mb-4 shadow-sm">
+      <img class="result-img" src="${result.artworkUrl600}" />
+      <div class="card-body">
+        <p class="card-text podcast-name">${result.collectionName}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <a href="${result.collectionViewUrl}" target="_blank">
+              <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+            </a>
           </div>
-          <div>
-          <i class="far fa-heart heart open-heart"></i>
-          </div>
+          <i class="far fa-heart heart"></i>
         </div>
       </div>
     </div>
-  `;
+  </div>`;
   $searchResults.append(temp);
   });
   console.log(res);
