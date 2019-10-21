f0029a4022e64c303bec0489eccf937207418cfe
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-10-21 15:48:37 -0700

    Add heart styling and the start of heart functionality

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -16,7 +16,11 @@ const onSuccess = (res) => {
               <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
             </a>
           </div>
-          <i class="far fa-heart heart"></i>
+          <i class="far fa-heart heart open-heart"
+          data-name="${result.collectionName}"
+          data-artist="${result.artistName}"
+          data-itunes-link="${result.collectionViewUrl}"
+          data-image-source="${result.artworkUrl600}"></i>
         </div>
       </div>
     </div>
@@ -39,4 +43,40 @@ $('form').on('submit', (e) => {
       console.log({err});
     }
   })
+});
+
+$('#searchResults').on('click', '.open-heart', function() {
+  $(this).removeClass('open-heart');
+  $(this).addClass('closed-heart');
+
+  $(this).removeClass('far');
+  $(this).addClass('fas');
+
+  // ajax call to create new podcast in User document
+  $.ajax({
+    method: 'POST',
+    url: 'https://localhost:4000/api/v1/podcasts',
+    data: {
+      name: $(this).data('name'),
+      artist: $(this).data('artist'),
+      itunesLink: $(this).data('itunes-link'),
+      imageSource: $(this).data('image-source'),
+    },
+    success: (req)=>{
+      console.log('success');
+    },
+    error: (err) => {
+      console.log(err);
+    }
+  });
+});
+
+$('#searchResults').on('click', '.closed-heart', function() {
+  $(this).removeClass('closed-heart');
+  $(this).addClass('open-heart');
+
+  $(this).removeClass('fas');
+  $(this).addClass('far');
+
+  // ajax call to delete podcast from User document
 });
